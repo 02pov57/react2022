abd9e2a4872596c22edc8210ec47bd2766bbc87d
Artem A I <artem-7779@yandex.ru>
2022-12-14 12:04:41 +0300

    css modules i s done

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,15 @@
 import App from "../App";
 import s from './Header.module.css';
 
-
 function Header(){
     return(   <div className={s.head}>
         <img src="https://halal-kz.kz/storage/app/media/logocomp-big.jpg" />
+
     </div>
-    );
+
+);
+    console.log(this.s);
+    console.log("cvbvjngvn");
 }
 
 export default Header;
